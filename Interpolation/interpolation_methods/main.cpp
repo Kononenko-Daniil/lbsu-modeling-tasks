13d4edf6c0a1c1eb5ab0b974820838dd720a663f
Kononenko-Daniil <neokv210@gmail.com>
2023-05-11 18:22:59 +0300

    - Interpolation - full - Approximation - mathematica - Body movement - student book exercises - Open all Excel files - Update Readme

--- Interpolation/interpolation_methods/main.cpp
+++ Interpolation/interpolation_methods/main.cpp
@@ -50,16 +50,71 @@ Data GetDataFromFile (AlgorithmType type) {
     return result;
 }
 
-vector<Coord>& LagrangeMethod (Data& data) {
+double LGetLx(vector<double>& xValues, double x, double inum) {
+    double res = 1, xi = xValues[inum];
+    for(int i = 0; i < xValues.size(); i++) {
+        if (inum != i) {
+            res *= (x - xValues[i]) / (xi - xValues[i]);
+        }
+    }
 
+    return res;
 }
 
-vector<Coord>& NewtonMethod (Data& data) {
+vector<Coord> LagrangeMethod (Data& data) {
+    vector<Coord> res;
+    for(int i = 0; i < data.desired_values.size(); i++) {
+        double y = 0, x = data.desired_values[i];
+        int n = data.x.size();
+        for(int j = 0; j < n; j++) {
+            y += data.y[j] * LGetLx(data.x, x, j);
+        }
 
+        Coord res_coord = { x, y };
+        res.push_back(res_coord);
+    }
+
+    return res;
 }
 
-void PrintResults(vector<Coord>& result) {
+vector<Coord> NewtonMethod (Data& data) {
+    vector<Coord> res;
+    int n = data.x.size();
+    for(int i = 0; i < data.desired_values.size(); i++) {
+        double x = data.desired_values[i];
+        vector<vector<double>> dy(n, vector<double>(n));
 
+        for (int i = 0; i < n; i++) {
+            dy[0][i] = data.y[i];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                dy[i][j] = dy[i-1][j+1] - dy[i-1][j];
+            }
+        }
+
+        double k = (x - data.x[0]) / 0.005, m = 1, f = 1;
+        double y = data.y[0];
+        for (int i = 1; i < n; i++) {
+            f *= i;
+            m *= (k - i + 1);
+            y += m / f * dy[i][0];
+        }
+
+        Coord res_coord = { x, y };
+        res.push_back(res_coord);
+    }
+
+    return res;
+}
+
+void PrintResults(vector<Coord>& result, string heading) {
+    int n = result.size();
+
+    cout << heading << endl;
+    for(int i = 0; i < n; i++) {
+        cout << "x: " << result[i].x << "; " << "y: " << result[i].y << endl;
+    }
 }
 
 int main () {
@@ -69,6 +124,8 @@ int main () {
     vector<Coord> lagrange_res = LagrangeMethod(lagrange_data);
     vector<Coord> newton_res = NewtonMethod(newton_data);
 
+    PrintResults(lagrange_res, "Lagrange method");
+    PrintResults(newton_res, "Newton method");
 
     return 0;
 }
